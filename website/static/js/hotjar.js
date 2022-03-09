e226fee7ee9de046b58508de9c689e1e24050886
Damián Parrino <bucanero@users.noreply.github.com>
2022-03-09 11:15:28 -0300

    Update hotjar.js

--- website/static/js/hotjar.js
+++ website/static/js/hotjar.js
@@ -1,7 +1,7 @@
-// Hotjar Tracking Code for nomicon.io
+// Hotjar Tracking Code for https://near-nodes.io
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2802987,hjsv:6};
+        h._hjSettings={hjid:2867520,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
